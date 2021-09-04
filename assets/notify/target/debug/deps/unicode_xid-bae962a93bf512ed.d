254d0e97512cfe82d5895122dfce12a6b030fcb1
Mateo Cruz <mateo.xzf@protonmail.ch>
2021-09-04 11:59:39 +0530

    remove assets/notify/ from git

--- assets/notify/target/debug/deps/unicode_xid-bae962a93bf512ed.d
+++
@@ -1,8 +0,0 @@
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\unicode_xid-bae962a93bf512ed.rmeta: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.0.4\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.0.4\src\tables.rs
-
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\libunicode_xid-bae962a93bf512ed.rlib: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.0.4\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.0.4\src\tables.rs
-
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\unicode_xid-bae962a93bf512ed.d: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.0.4\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.0.4\src\tables.rs
-
-C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.0.4\src\lib.rs:
-C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.0.4\src\tables.rs:
--- assets/notify/target/debug/deps/winrt_notification-71706f2e90654230.d
+++
@@ -1,8 +0,0 @@
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\winrt_notification-71706f2e90654230.rmeta: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\winrt-notification-0.2.4\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\winrt-notification-0.2.4\src\windows_check.rs
-
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\libwinrt_notification-71706f2e90654230.rlib: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\winrt-notification-0.2.4\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\winrt-notification-0.2.4\src\windows_check.rs
-
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\winrt_notification-71706f2e90654230.d: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\winrt-notification-0.2.4\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\winrt-notification-0.2.4\src\windows_check.rs
-
-C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\winrt-notification-0.2.4\src\lib.rs:
-C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\winrt-notification-0.2.4\src\windows_check.rs:
