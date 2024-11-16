f1d790ba578ddcbfd76afd5cb851eb0c4331e010
Aditya <901aditya109@gmail.com>
2024-11-16 12:36:26 +0530

    remove assets/notify/ from git

--- assets/notify/target/debug/deps/synom-89fb531114d0b55b.d
+++
@@ -1,9 +0,0 @@
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\synom-89fb531114d0b55b.rmeta: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\space.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\helper.rs
-
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\libsynom-89fb531114d0b55b.rlib: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\space.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\helper.rs
-
-E:\Dox\Code\Projects\bannoti\notify\target\debug\deps\synom-89fb531114d0b55b.d: C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\lib.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\space.rs C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\helper.rs
-
-C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\lib.rs:
-C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\space.rs:
-C:\Users\mateo\.cargo\registry\src\github.com-1ecc6299db9ec823\synom-0.11.3\src\helper.rs:
--- assets/notify/target/debug/notify.d
+++
@@ -1,1 +0,0 @@
-E:\Dox\Code\Projects\bannoti\notify\target\debug\notify.exe: E:\Dox\Code\Projects\bannoti\notify\src\main.rs
