05481aa7adf6ac53cead2b23f1986902ab1d9e64
Aditya <901aditya109@gmail.com>
2024-11-16 12:36:23 +0530

    add help & banner :)

--- bannoti.py
+++ bannoti.py
@@ -1,15 +1,55 @@
 # Amazing! Core functionality is working great, and the notifications look nice now.
 # note : polish interface & file paths
 
+from sys import exit as die
+from sys import argv
+from colorama import Fore, Style, init
+init()
+
+
+def show_help() -> None:
+    print("""
+    bannoti.py <option>
+
+    run : run the program
+    --add : add an address to get notified for
+    --remove : stop getting notifications for an address
+    --gettracked : get tracked accounts
+
+    If this is your first time using Bannoti, I recommend you see the README (./README.md).""")
+
+print(f"""{Fore.BLUE}
+
+                     ____                          _   _
+                     | __ )  __ _ _ __  _ __   ___ | |_(_)
+                     |  _ \ / _` | '_ \| '_ \ / _ \| __| |
+                     | |_) | (_| | | | | | | | (_) | |_| |
+                     |____/ \__,_|_| |_|_| |_|\___/ \__|_|
+
+                                        {Fore.YELLOW}Desktop notifier for banano{Style.RESET_ALL} ({Fore.YELLOW}https://banano.cc{Style.RESET_ALL})
+                                        {Fore.YELLOW} by Mateo Cruz {Style.RESET_ALL} ({Fore.YELLOW}https://github.com/wolfrust{Style.RESET_ALL})
+
+
+""")
+
+try:
+    argv[1]
+except IndexError:
+    show_help()
+    die()
+
+if argv[1] in ['--help', '-h', 'help'] or argv[1] not in ['--add', 'run', '--remove', '--gettracked']:
+    show_help()
+    die()
+
 from bananopy import banano as ban
 from time import sleep
 from datetime import datetime
 import track
-from sys import argv
 from os import system as exec
 from platform import system as getOS
-from colorama import Fore, Style, init
-init()
+
+print()
 
 def is_negative(number):
     if ((0 + number) > 0):
